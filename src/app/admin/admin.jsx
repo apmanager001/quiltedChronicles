ad9399a56e9e3610086b4a368e0e8f985bddb57b
apmanager <121115177+apmanager001@users.noreply.github.com>
2025-01-27 14:13:22 -0500

    updated admin expand

--- src/app/admin/admin.jsx
+++ src/app/admin/admin.jsx
@@ -1,5 +1,6 @@
 'use client'
 import React, {useState} from 'react'
+import Expanded from '../chain/comp/expand';
 import Link from 'next/link';
 import Flag from './flag/flag';
 import Messages from './messages/messages';
@@ -18,39 +19,42 @@ const Admin = () => {
 
   return (
     <AccountPage>
-    <div className="w-full min-h-96">
-      <div role="tablist" className="tabs tabs-boxed">
-        <Link
-          href="#"
-          role="tab"
-          className={`tab ${activeTab === "Flag" ? "tab-active" : ""}`}
-          onClick={() => handleContentChange("Flag")}
-        >
-          Flag
-        </Link>
-        <Link
-          href="#"
-          role="tab"
-          className={`tab ${activeTab === "Messages" ? "tab-active" : ""}`}
-          onClick={() => handleContentChange("Messages")}
-        >
-          Messages
-        </Link>
-        <Link
-          href="#"
-          role="tab"
-          className={`tab ${activeTab === "Stats" ? "tab-active" : ""}`}
-          onClick={() => handleContentChange("Stats")}
-        >
-          Stats
-        </Link>
+      <div className="w-full min-h-96">
+        <div className='flex justify-between items-center w-full gap-4'>
+          <div role="tablist" className="tabs tabs-boxed flex-grow ">
+            <Link
+              href="#"
+              role="tab"
+              className={`tab ${activeTab === "Flag" ? "tab-active" : ""}`}
+              onClick={() => handleContentChange("Flag")}
+            >
+              Flag
+            </Link>
+            <Link
+              href="#"
+              role="tab"
+              className={`tab ${activeTab === "Messages" ? "tab-active" : ""}`}
+              onClick={() => handleContentChange("Messages")}
+            >
+              Messages
+            </Link>
+            <Link
+              href="#"
+              role="tab"
+              className={`tab ${activeTab === "Stats" ? "tab-active" : ""}`}
+              onClick={() => handleContentChange("Stats")}
+            >
+              Stats
+            </Link>
+          </div>
+          <Expanded />
+        </div>
+        <div>
+          {visibleComponent === "Flag" && <Flag />}
+          {visibleComponent === "Messages" && <Messages />}
+          {visibleComponent === "Stats" && <Stats />}
+        </div>
       </div>
-      <div>
-        {visibleComponent === "Flag" && <Flag />}
-        {visibleComponent === "Messages" && <Messages />}
-        {visibleComponent === "Stats" && <Stats />}
-      </div>
-    </div>
     </AccountPage>
   );
 }
